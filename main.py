5b21a648f3f5e15dbb9e60650f9fb5a2bddf8fe9
Vampaxx <arjunappu1001@gmail.com>
2024-01-07 00:01:54 +0530

    logging_setup

---
+++ main.py
@@ -0,0 +1,6 @@
+from  src.text_summarization.logging import logger
+
+
+
+
+logger.info("here is my custom logging")
--- setup.py
+++ setup.py
@@ -1,33 +1,16 @@
 import setuptools
-from setuptools import find_packages
-from typing import List
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    
-    return requirements
-
 
 __version__ = "0.0.0"
 
 REPO_NAME = "text_summarization"
 AUTHOR_USER_NAME = "Vampaxx"
-SRC_REPO = "text_summarization"
+SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "arjunappu1001@gmail.com"
 
 
@@ -41,7 +24,9 @@ setuptools.setup(
     long_description_content = "text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "text summarization" : f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker" : f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt'))
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"))
+
+
